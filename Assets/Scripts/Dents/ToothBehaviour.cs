933425b40172060f9e488015564a6c6f937d5e10
Marie Nuellas <m.nuellas@gmail.com>
2023-02-05 01:50:46 +0100

    DENT : faire trembler la dent

--- Assets/Scripts/Dents/ToothBehaviour.cs
+++ Assets/Scripts/Dents/ToothBehaviour.cs
@@ -12,6 +12,17 @@ public class ToothBehaviour : MonoBehaviour
     int tempsDArrachage = 5;
     Vector3 mousePosition;
     float localTempsArrache;
+    [SerializeField]
+    float speed = 1.0f; //how fast it shakes
+    [SerializeField]
+    float amount = 1.0f; //how much it shakes
+    Vector3 originalPosition;
+    float tempsPasse = 0;
+    bool tooLate = false;
+
+    private void Start() {
+        originalPosition = transform.position;
+    }
 
     private void OnMouseDown() {
         GameObject.Find("GameManager");
@@ -23,23 +34,44 @@ public class ToothBehaviour : MonoBehaviour
     }
 
     private void OnMouseDrag() {
-        var heading = Input.mousePosition - mousePosition;
-        var dragPosition = heading / heading.magnitude;
-        if (heading.magnitude > minMagnitude) {
-            if ((dragPosition.y > 0 && !retournee) || (dragPosition.y < 0 && retournee)) {
-                localTempsArrache -= Time.deltaTime;
-                if (localTempsArrache < 0) {
-                    if (isBad)
-                        print("Win !!!");
-                    else
-                        print("LOSE");
+        if (!tooLate) {
+            var heading = Input.mousePosition - mousePosition;
+            var dragPosition = heading / heading.magnitude;
+            Shake(localTempsArrache);
+            if (heading.magnitude > minMagnitude) {
+                if ((dragPosition.y > 0 && !retournee) || (dragPosition.y < 0 && retournee)) {
+                    localTempsArrache -= Time.deltaTime;
+                    if (localTempsArrache < 0) {
+                        tooLate = true;
+                        if (retournee)
+                            transform.position += new Vector3(0, -1, 0);
+                        else
+                            transform.position += new Vector3(0, 1, 0);
+                        if (isBad) //TODO changer en invoke
+                            print("Win !!!");
+                        else
+                            print("LOSE");
+                    }
                 }
-            }
 
+            }
         }
+
+    }
+
+    void Shake(float localTime) {
+        var xOffset = Mathf.PerlinNoise ( localTime * speed, 0 );
+        var yOffset = Mathf.PerlinNoise ( 0, localTime * speed );
+
+        transform.position = originalPosition + new Vector3 ( xOffset, yOffset, 0 ) * amount;
     }
 
     private void OnMouseUp() {
         localTempsArrache = tempsDArrachage;
+        transform.position = originalPosition;
+    }
+    
+    private void FixedUpdate() {
+        tempsPasse += Time.deltaTime;
     }
 }
